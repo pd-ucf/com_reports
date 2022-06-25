cea10194589ebf3840f1e4735ede6135ee240f03
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-06-25 12:20:47 -0400

    History: ID + Week Search Update
    -Report history works with the new ID system
    -Can now search report history by report week

--- site/tmpl/history/default.php
+++ site/tmpl/history/default.php
@@ -2,30 +2,67 @@
 defined('_JEXEC') or die('Restricted Access');
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Uri\Uri;
 
-$weekStart = date_create()->modify('Last Sunday')->format('Y-m-d');
-$displayWeek = date_create()->modify('Last Sunday')->format('n/j/Y');
+$uri = Uri::getInstance();
+$date_param = date_create($uri->getVar('date')) ?? date_create();
+$date_param->modify('Last Sunday');
+
+$weekStart = $date_param->format('Y-m-d');
+$displayWeek = $date_param->format('n/j/Y');
 $db = JFactory::getDbo();
 
 $query = $db->getQuery(true);
-$query->select(array('data', 'username'))->from('reports')->where("weekStart = '$weekStart'");
+$query->select(array('data', 'id'))->from('reports')->where("weekStart = '$weekStart'");
 $db->setQuery($query);
 $pastSubs = $db->loadRowList();
 
+if(array_key_exists('search', $_POST)) {
+  $app = Factory::getApplication();
+  $input = $app->input;
+  $app->redirect(JRoute::_(JURI::current() . "?view=history&date=" . $input->get('date_param')));
+}
+
 ?>
 <h1>Reports of the Week - <?= $displayWeek;?></h1>
-<h2><?php if(!isset($pastSubs)) echo "There are no reports here"?></h2>
+
+<style>
+    .filters {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .search-bar {
+        width: 310px;
+    }
+</style>
+
+<form method="post" class="filters"> 
+    <div class="search-bar input-group"> 
+        <input name="date_param" class="form-control" value="<?php echo $displayWeek ?>" type="date"/>
+        <input type="submit" name="search" value="Search" class="btn btn-secondary" />
+        <a class="btn btn-danger" href="<?php echo JURI::current(); ?>?view=history">Clear</a>
+    </div>
+</form>
+
+<h3><?php if(count($pastSubs) == 0) echo "No reports found"?></h3>
+
 <div class="accordion accordion-flush" id="accordionFlushExample">
   <?php
-  $reportNum = 1;
+  $accordionNum = 1;
   foreach ($pastSubs as $prevReport) {
     $data = json_decode($prevReport[0], true);
-    $username = $prevReport[1];
+    $name = Factory::getUser($prevReport[1])->name;
+    $numReports = count($data) / 4;
+    $reportMessage = $numReports == 1 ? "$numReports Report" : "$numReports Reports";
 
     echo  '<div class="accordion-item">';
 
-    echo "<h2 class='accordion-header' id='flush-heading$reportNum'> <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#flush-collapse$reportNum' aria-expanded='false' aria-controls='flush-collapse$reportNum'> $username's Reports</button> </h2>";
-    echo "<div class='accordion-collapse collapse' id='flush-collapse$reportNum' aria-labelledby='flush-heading$reportNum' data-bs-parent='#accordionFlushExample'> <div class='accordion-body'>";
+    echo "<h2 class='accordion-header' id='flush-heading$accordionNum'> <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#flush-collapse$accordionNum' aria-expanded='false' aria-controls='flush-collapse$accordionNum'> $name - $reportMessage</button> </h2>";
+    echo "<div class='accordion-collapse collapse' id='flush-collapse$accordionNum' aria-labelledby='flush-heading$accordionNum' data-bs-parent='#accordionFlushExample'> <div class='accordion-body'>";
 
     echo "<table class='table table-striped'> <thead> <tr> <th style='width: 50px;' scope='col'>#</th> <th scope='col'>Date/Time</th> <th scope='col'>Minutes Spent</th> <th scope='col'>Type</th> <th scope='col'>Description</th> </tr> </thead> <tbody>";
 
@@ -50,7 +87,7 @@ $pastSubs = $db->loadRowList();
     echo '</tbody> </table>';
     echo '</div> </div> </div>';
 
-    $reportNum++;
+    $accordionNum++;
   }
   ?>
 
