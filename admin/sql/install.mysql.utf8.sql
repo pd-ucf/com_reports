9df9697d4dd67b83518991b704f70cc8606e0e35
pd-ucf <70347586+pd-ucf@users.noreply.github.com>
2022-07-18 10:46:20 -0400

    Update install.mysql.utf8.sql

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS reports;
 
 CREATE TABLE `reports` (
+        `pk` SERIAL NOT NULL AUTO_INCREMENT,
 	`id` SERIAL NOT NULL,
 	`weekStart` VARCHAR(20) NOT NULL,
-	`data` JSON NOT NULL
+	`data` JSON NOT NULL,
+	PRIMARY KEY (`pk`)
 ) ENGINE=InnoDB;
