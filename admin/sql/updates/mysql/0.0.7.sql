035714aa278be96a6b66ddc3692a7348d158d684
pd-ucf <70347586+pd-ucf@users.noreply.github.com>
2022-07-18 10:59:10 -0400

    Update 0.0.7.sql

--- admin/sql/updates/mysql/0.0.7.sql
+++ admin/sql/updates/mysql/0.0.7.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS reports;
 
 CREATE TABLE `reports` (
-        `pk` SERIAL NOT NULL AUTO_INCREMENT,
-	`id` SERIAL NOT NULL,
+        `pk` SERIAL PRIMARY KEY NOT NULL,
+	`id` INT NOT NULL,
 	`weekStart` VARCHAR(20) NOT NULL,
-	`data` JSON NOT NULL,
-	PRIMARY KEY (`pk`)
+	`data` JSON NOT NULL
 ) ENGINE=InnoDB;
