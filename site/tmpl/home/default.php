9bd3dc4a19b302a45e0a1cefbbc66c717cfda8e1
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-07-25 19:13:23 -0400

    Update default.php

--- site/tmpl/home/default.php
+++ site/tmpl/home/default.php
@@ -56,6 +56,8 @@ $pastSubs = $db->loadRowList();
     }
 </style>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 <div class="mx-auto" style="width: 250px;margin-bottom: 20px">
   <button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='index.php?option=com_reports&view=create'">New Weekly Report</button>
 </div>
