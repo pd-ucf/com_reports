4b850d0e26bf5d150b1e7a92ff4de04d31b56764
Namelessfury <69160890+Namelessfury@users.noreply.github.com>
2022-07-25 17:17:29 -0400

    Update default.php

--- site/tmpl/home/default.php
+++ site/tmpl/home/default.php
@@ -56,7 +56,6 @@ $pastSubs = $db->loadRowList();
     }
 </style>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <div class="mx-auto" style="width: 250px;margin-bottom: 20px">
